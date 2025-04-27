297f653e9d3cd9dd776bee4f54f5c516ab4d7323
Janek <75185339+213N10@users.noreply.github.com>
2025-04-27 22:05:57 +0200

    Init message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { AuthService } from './auth.service';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  template:`
+  <h1>{{title}}</h1>
+      <nav class="navbar">
+      <div class="nav-left">
+        <a routerLink="/">Report a Violation</a>
+      </div>
+      <div class="nav-right">
+        @if (!isLoggedIn) {
+          <a routerLink="/register" routerLinkActive="active" ariaCurrentWhenActive="page">Register</a>
+          <a routerLink="/login" routerLinkActive="active" ariaCurrentWhenActive="page">Login</a>   
+        } @else {
+          <a routerLink="/users-search" routerLinkActive="active" ariaCurrentWhenActive="page">Users search</a>
+          <button (click)="logout()">Logout</button>
+        }
+      </div>
+    </nav>
+  <router-outlet></router-outlet>
+
+  
+  `,
+  styles: `
+    .navbar {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1rem;
+      background: #e0e0e0;
+    } 
+    .nav-left a, .nav-right a, .nav-right button {
+      margin: 0 0.5rem;
+      text-decoration: none;
+      color: black;
+      font-weight: bold;
+      background: none;
+      border: none;
+      cursor: pointer;
+    }
+  `
 })
 export class AppComponent {
-  title = 'note-sharing-app-frontend';
+  title = 'Note Sharing App';
+  isLoggedIn = false;
+  user: any = null 
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit() {
+    this.authService.loggedIn$.subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
+
+  logout() {
+    this.authService.logout(); 
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
+  },
+  {
+    path: 'users-search',
+    component: UserSearchComponent,
+    title: 'User Search',
+  },
+  {
+    path: 'users/:id',
+    component: UserDetailsComponent,
+    title: 'User Details',
+  }
+];
